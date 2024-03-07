66f6a4b7e471bda6df790c5c9f8e20a5813a4b71
Shailesh pandey <pandeyshailesh2081998@gmail.com>
2024-03-07 15:41:01 +0530

    created the courses section

---
+++ client/src/resources/courses.js
@@ -0,0 +1,42 @@
+export const courses = [
+    {
+        _id:"360120804042e2d798b59cf",
+        title:"React Development Cources",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0J7z3hh5CV3BznfGuD1Kabw4MCfAveAoWIQ&usqp=CAU",
+        link:"/",
+        description:"An Ecommerce website developed by  using React Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+        _id:"360120804042e2d798b59cr",
+        title:"java Development Cources",
+        image:
+        "https://image.spreadshirtmedia.com/image-server/v1/compositions/T210A1PA4301PT17X40Y61D1016839396W25007H14510/views/1,width=550,height=550,appearanceId=1,backgroundColor=FFFFFF,noPt=true/i-love-java-hello-world-geschenkidee-mens-t-shirt.jpg",
+        link:"/",
+        description:"An Ecommerce website developed by  using React Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+        _id:"360120804042e2d798b59ct",
+        title:"JS Development Cources",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBaV3IhV_gHjp3IMbN1laHqkURMQ-ZHQVGBg&usqp=CAU",
+        link:"/",
+        description:"An Ecommerce website developed by  using React Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+        _id:"360120804042e2d798b59cg",
+        title:"Web 3 Development Cources",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zJGjIz9kDavlUZsrJCE2Rgk5peTxnazBvg&usqp=CAU",
+        link:"/",
+        description:"An Ecommerce website developed by  using React Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+        _id:"360120804042e2d798b59ch",
+        title:"mongoDb Development Cources",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3TqStfKDtmGzs404YG7_j5xSMReY1kSmVeg&usqp=CAU",
+        link:"/",
+        description:"An Ecommerce website developed by  using React Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+]
