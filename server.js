058b3e8bd106b13e92411b7b52f02330b3624aeb
Shailesh pandey <pandeyshailesh2081998@gmail.com>
2024-03-09 16:12:17 +0530

    hosting part

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 
 const PORT = process.env.PORT || 5000
+const path = require("path");
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"client/build")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"client/build/index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`server is Up on Running at ${PORT}`);
 })
